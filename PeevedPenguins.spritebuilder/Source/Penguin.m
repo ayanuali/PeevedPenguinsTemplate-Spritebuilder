ecdc8a22682e43604b3b91489acd8607a8a6c8c8
ayan <stormfrog.ayan@gmail.com>
2014-07-21 23:32:38 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -9,5 +9,14 @@
 #import "Penguin.h"
 
 @implementation Penguin
-
+-(id)init{
+    self = [super init];
+    
+    if(self)
+    {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
 @end
