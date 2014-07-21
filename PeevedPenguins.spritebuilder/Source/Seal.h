0bc03e011ef6aea061b9ef5f96e8123a9d6d5d3d
ayan <stormfrog.ayan@gmail.com>
2014-07-21 23:21:31 +0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Ayan Uali on 7/21/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Ayan Uali on 7/21/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
