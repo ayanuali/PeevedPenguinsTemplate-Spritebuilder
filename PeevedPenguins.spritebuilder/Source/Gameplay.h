b33aa637c0cb5cdbb3cbfbeacd89b7f9557bcc79
ayan <stormfrog.ayan@gmail.com>
2014-07-27 10:55:01 +0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Ayan Uali on 7/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,16 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Ayan Uali on 7/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay{
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+@end
