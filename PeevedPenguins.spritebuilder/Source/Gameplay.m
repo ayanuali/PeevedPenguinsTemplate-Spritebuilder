714428898768d4ac8914fb2ec4d72e0a311952d4
ayan <stormfrog.ayan@gmail.com>
2014-08-01 16:12:19 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -161,14 +161,11 @@
 -(void)sealRemoved:(CCNode *)seal
 {
     // load particle effect
-    CCParticleSystem *explosion = ((CCParticleSystem *)[CCBReader load:@"SealExplosion"]);
-    
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
     // make the particle effect clean itself up, once it is completed
     explosion.autoRemoveOnFinish = TRUE;
-    
     // place the particle effect on the seals position
     explosion.position = seal.position;
-    
     // add the particle effect to the same node the seal is on
     [seal.parent addChild:explosion];
     
