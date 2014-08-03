b46ad43064ece58cfb94990beb91af471ea2eaa3
ayan <stormfrog.ayan@gmail.com>
2014-08-03 22:49:43 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,6 +43,37 @@ static const float MIN_SPEED = 5.f;
     
 }
 
+-(void)update:(CCTime)delta
+{
+     // if speed is below minimum speed, assume this attempt is over
+    if(ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED)
+    {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMin = _currentPenguin.boundingBox.origin.x;
+    
+    if(xMin < self.boundingBox.origin.x)
+    {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+    if(xMax > (self.boundingBox.origin.x + self.boundingBox.size.width))
+    {
+        [self nextAttempt];
+        return;
+    }
+}
+
+-(void)nextAttempt
+{
+    
+}
+
 //// called on every touch in this scene
 //-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 //{
